44ad8c73940fc4ea0e3a1c62c6c53208f5ffbddd
GodHongIl <adunstudio@daum.net>
2025-01-21 17:26:46 +0900

    리팩토링

--- 6/base.py
+++ 6/base.py
@@ -18,12 +18,13 @@ class ImageChatService:
         self.st = st
 
         self.st.session_state.setdefault("waiting", False)
-        self.st.session_state.setdefault("messages", [
-            SystemMessage(content='당신은 주어지는 이미지를 참고해서 응답하는 챗봇입니다.'),
-            AIMessage(content='안녕하세요! 무엇을 도와드릴까요?')
-        ])
+        self.st.session_state.setdefault("image_messages", [])
+        self.st.session_state.setdefault("common_messages", [])
 
-        self.messages = self.st.session_state.messages
+        self.system_messages = [SystemMessage(content='당신은 주어지는 이미지를 참고해서 응답하는 챗봇입니다.')]
+        self.image_messages  = self.st.session_state.image_messages
+        self.common_messages = self.st.session_state.common_messages
+
 
         self.llm = ChatOpenAI(model="gpt-4o-mini", api_key=api_key)
 
@@ -34,9 +35,16 @@ class ImageChatService:
     def set_waiting(self, waiting: bool):
         self.st.session_state.waiting = waiting
 
+    @property
+    def have_message(self):
+        return len(self.common_messages) > 0
+
+    def add_image(self, image):
+        base64_image = base64.b64encode(image.read()).decode("utf-8")
+        self._add_image_message(base64_image)
 
 
-    def add_image(self, base64_image):
+    def _add_image_message(self, base64_image):
         image_message = HumanMessage(
             content=[
                 {
@@ -45,42 +53,50 @@ class ImageChatService:
                 },
             ],
         )
-        self.messages.append(image_message)
+        self.image_messages.append(image_message)
+
+    def _add_human_message(self, prompt):
+        self.common_messages.append(HumanMessage(content=prompt))
+
+    def _add_ai_message(self, prompt):
+        self.common_messages.append(AIMessage(content=prompt))
+
+    @property
+    def _messages(self):
+        return self.system_messages + self.image_messages + self.common_messages
 
     def answer_generate(self, prompt):
-        self.messages.append(HumanMessage(content=prompt))
+        self._add_human_message(prompt)
 
-        result = self.llm.invoke(self.messages)
-
+        result = self.llm.invoke(self._messages)
         response = result.content
-        self.messages.append(AIMessage(content=response))
+
+        self._add_ai_message(result)
+
         return response
 
     def answer_generate_stream(self, prompt):
-        self.messages.append(HumanMessage(content=prompt))
+        self._add_human_message(prompt)
 
-        result_stream = self.llm.stream(self.messages)
+        result_stream = self.llm.stream(self._messages)
 
         response = ""
         for chunk in result_stream:
             response += chunk.content
             yield chunk.content
 
-        self.messages.append(AIMessage(content=response))
+        self._add_ai_message(response)
 
 
 def main():
     chat_service = ImageChatService(st, Config.OPENAI_API_KEY)
 
-    have_human_message = False
-
     st.title("Image Chat Bot")
 
     if uploaded_images := st.file_uploader("함께할 이미지를 올려주세요!", type=['png', 'jpg', 'jpeg'], accept_multiple_files=True):
-        # 업로드된 파일 처리
+
         for uploaded_image in uploaded_images:
-            base64_image = base64.b64encode(uploaded_image.read()).decode("utf-8")
-            chat_service.add_image(base64_image)
+            chat_service.add_image(uploaded_image)
 
         image_count = len(uploaded_images)
         cols = st.columns(image_count)
@@ -89,29 +105,17 @@ def main():
             with col:
                 st.image(uploaded_images[idx], use_container_width=True, caption=uploaded_images[idx].name)
 
-        for message in chat_service.messages:
-            role = 'system' if isinstance(message, SystemMessage) else \
-                    'ai' if isinstance(message, AIMessage) else \
-                    'human'
+        with st.chat_message('ai'):
+            st.markdown('안녕하세요. 무엇을 도와드릴까요?')
 
-            if role == 'system':
-                # 시스템 메시지는 무시
-                continue
-
-            if isinstance(message.content, list):
-                # 이미지 메시지는 무시
-                continue
-
-            if role == 'human':
-                have_human_message = True
+        for message in chat_service.common_messages:
+            role = 'ai' if isinstance(message, AIMessage) else 'human'
 
             with st.chat_message(role):
                 st.markdown(message.content)
 
-
         selected_prompt = None
-
-        if have_human_message == False:
+        if not chat_service.have_message:
             left, right = st.columns(2)
 
             if left.button("주어진 두 사진의 공통점이 뭐야?", use_container_width=True, disabled=chat_service.waiting, on_click=chat_service.set_waiting, args=[True]):
@@ -133,6 +137,7 @@ def main():
 
                 chat_service.set_waiting(False)
                 st.rerun()
+
 
 if __name__ == "__main__":
     main()
