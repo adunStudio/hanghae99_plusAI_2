eabe4b73f3e0d5d6359bcbbb13b435b0cd51a603
GodHongIl <adunstudio@daum.net>
2025-01-22 18:27:30 +0900

    파일명 변경(base->basic)

--- 6/base_lib.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-# 파일의 해시 값 계산
-def calculate_file_hash(file):
-    hasher = hashlib.md5()
-    hasher.update(file.read())
-    file.seek(0)
-    return hasher.hexdigest()
--- 6/basic.py
+++ 6/basic.py
@@ -26,29 +26,29 @@ def main():
 
     if uploaded_images := st.file_uploader("✨ 함께할 이미지를 올려볼까요? 🌈", type=['png', 'jpg', 'jpeg'], accept_multiple_files=True):
 
-        # 🔹 이미지 업로드
+        # 🔹 1. 이미지 업로드
         for uploaded_image in uploaded_images:
             chat_service.add_image(uploaded_image)
 
         cols = st.columns(len(uploaded_images))
 
-        # 🔹 이미지 노출
+        # 🔹 2. 이미지 노출
         for idx, col in enumerate(cols):
             with col:
                 st.image(uploaded_images[idx], use_container_width=True, caption=uploaded_images[idx].name)
 
-        # 🔹 시작 메시지
+        # 🔹 3. 시작 메시지
         with st.chat_message('ai'):
             st.markdown('💡 **안녕하세요!** 이미지를 보면서 이야기를 나눠봐요. 궁금한 점을 물어보세요! 😊')
 
-        # 🔹 채팅 메시지
+        # 🔹 4. 채팅 메시지 히스토리
         for message in chat_service.chat_histories:
             role = 'ai' if isinstance(message, AIMessage) else 'human'
 
             with st.chat_message(role):
                 st.markdown(message.content)
 
-        # 🔹 메시지 선택
+        # 🔹5. 기존에 메시지 없다면 -> 준비된 목록 선택 가능
         selected_prompt = None
         if not chat_service.have_message:
             prepared_title = ['🤔 **두 사진의 공통점은?**', '🔍 **두 사진의 차이점은?**']
@@ -59,13 +59,13 @@ def main():
                                 on_click=chat_service.set_waiting, args=[True]):
                     selected_prompt = prepared_prompt[idx]
 
-        # 🔹 메시지 입력
+        # 🔹 6. 메시지 직접 입력
         prompt = st.chat_input("📝 메시지를 입력해보세요! 😊", disabled=chat_service.waiting, on_submit=chat_service.set_waiting(True))
         if selected_prompt or prompt:
             with st.chat_message("user"):
                 st.markdown(selected_prompt or prompt)
 
-            # 🔹 메시지 요청 & 답변
+            # 🔹 7. 메시지 요청 & 답변
             with st.chat_message("assistant"):
                 st.write_stream(chat_service.answer_generate_stream(selected_prompt or prompt))
                 # st.markdown(chat_service.answer_generate(selected_prompt or prompt))
