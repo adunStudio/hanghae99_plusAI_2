879aec47b825c839c343d89e83aabc190050bdb7
GodHongIl <adunstudio@daum.net>
2025-01-22 18:41:25 +0900

    요약은 다른 서비스로

--- 6/service/image_chat_service.py
+++ 6/service/image_chat_service.py
@@ -11,37 +11,20 @@ class ImageChatService:
         self._api_key = api_key
         self._summary_max_token = summary_max_token
 
-        # 한글 요약 프롬프트 정의d
-        summary_prompt = PromptTemplate(
-            input_variables=["summary", "new_lines"],
-            template=(
-                "다음은 지금까지의 대화 요약입니다:\n"
-                "{summary}\n\n"
-                "다음은 새로 추가된 대화입니다:\n"
-                "{new_lines}\n\n"
-                "이 대화를 기반으로 대화 요약을 한글로 업데이트하세요:"
-            ),
-        )
-
-        '''
-        self.st.session_state.setdefault("common_memory", ConversationSummaryBufferMemory(
-            llm=ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=api_key),
-            max_token_limit=self._summary_max_token,
-            #prompt=summary_prompt,
-            verbose=False
-        ))
-        '''
-
         self._llm = ChatOpenAI(model="gpt-4o-mini", api_key=api_key)
 
-        self._system_messages    = [SystemMessage(content='당신은 주어지는 이미지를 참고해서 응답하는 챗봇입니다.')]
-        self._image_messages     = []
-        self._common_messages    = []
-        self._common_memory      = []
-        self._summary_messages   = []
-        self._truncated_messages = []
+        self._system_messages = [SystemMessage(content='당신은 주어지는 이미지를 참고해서 응답하는 챗봇입니다.')]
+        self._image_messages  = []
+        self._common_messages = []
 
         self._waiting = False
+
+    ####################################################################################################################
+    # Public Method
+    ####################################################################################################################
+    def add_image(self, image):
+        base64_image = base64.b64encode(image.read()).decode("utf-8")
+        self._add_image_message(base64_image)
 
     def answer_generate(self, prompt):
         self._add_human_message(prompt)
@@ -51,32 +34,30 @@ class ImageChatService:
 
         self._add_ai_message(response)
 
-        #self._on_tokens_changed(callback.total_tokens)
-
         return response
-
 
     def answer_generate_stream(self, prompt):
         self._add_human_message(prompt)
 
         result_stream = self._llm.stream(self._all_messages)
 
-        response = ""
+        response = ''
         for chunk in result_stream:
             response += chunk.content
             yield chunk.content
 
         self._add_ai_message(response)
 
-        # self._on_tokens_changed(callback.total_tokens)
+    def set_waiting(self, waiting: bool):
+        self._waiting = waiting
 
     @property
     def waiting(self):
         return self._waiting
 
-    def set_waiting(self, waiting: bool):
-        self._waiting = waiting
-
+    ####################################################################################################################
+    # Property
+    ####################################################################################################################
     @property
     def have_message(self):
         return len(self._common_messages) > 0
@@ -87,11 +68,7 @@ class ImageChatService:
 
     @property
     def _all_messages(self):
-        return self._system_messages + self._image_messages + self._summary_messages + self._truncated_messages
-
-    def add_image(self, image):
-        base64_image = base64.b64encode(image.read()).decode("utf-8")
-        self._add_image_message(base64_image)
+        return self._system_messages + self._image_messages + self._common_messages
 
     ####################################################################################################################
     # Private Method
@@ -110,42 +87,7 @@ class ImageChatService:
     def _add_human_message(self, prompt):
         human_message = HumanMessage(content=prompt)
         self._common_messages.append(human_message)
-        self._truncated_messages.append(human_message)
 
     def _add_ai_message(self, prompt):
         ai_message = AIMessage(content=prompt)
         self._common_messages.append(ai_message)
-        self._truncated_messages.append(ai_message)
-
-
-    def _on_tokens_changed(self, tokens):
-        return
-        print(tokens)
-
-        # 토큰 수가 임계값을 넘지 않으면 요약하지 않음
-        if tokens < self._summary_max_token:
-            return
-
-        # _truncated_messages가 비어 있으면 요약하지 않음
-        if len(self._truncated_messages) == 0:
-            return
-
-
-        print('요약 시작')
-        previous_summary = ''
-        if len(self._summary_messages) != 0:
-            print("기존 요약 있다.")
-            previous_summary = self._summary_messages[0].content
-
-        new_summary_message = self._common_memory.predict_new_summary(self._truncated_messages, previous_summary)
-
-        self._truncated_messages.clear()
-
-        self._summary_messages = [AIMessage(content=f'{new_summary_message}')]
-
-        print(f'이전 대화 요약: {new_summary_message}')
-
-
-
-
-
