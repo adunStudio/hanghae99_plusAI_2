d365c645533ee7684bd62971471b0cfefeb8ce46
GodHongIl <adunstudio@daum.net>
2025-02-08 03:25:28 +0900

    최종일까요

--- 7주차_심화/train.py
+++ 7주차_심화/train.py
@@ -8,7 +8,7 @@ import wandb
 
 # 🔹 1. 데이터 로드 및 준비
 # corpus.json 파일 로드
-with open("corpus2.json", "r", encoding="utf-8") as file:
+with open("corpus.json", "r", encoding="utf-8") as file:
     corpus_data = json.load(file)
 
 # 🔹 2. 학습/검증 데이터 분할
@@ -21,10 +21,7 @@ dataset = DatasetDict({
 })
 
 # 🔹 4. 모델 및 토크나이저 설정
-#model_name = "meta-llama/Llama-3.2-3B-Instruct"
-#model_name = "google/gemma-2-2b-it"
-#model_name = "facebook/opt-350m"
-model_name = "meta-llama/Llama-3.2-1B-Instruct"
+model_name = "facebook/opt-350m"
 model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -41,32 +38,50 @@ response_template = " ### Description:"
 collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
 
 # 🔹 7. W&B 설정
-wandb.init(project="instruction_tuning", name="Llama-3.2-1B-Instruct")
+wandb.init(project="facebook_opt_instruction_tuning", name=model_name)
 
+# 🔹 8. 로그 함수
+from transformers import TrainerCallback, TrainerState, TrainerControl
+class WandbLoggingCallback(TrainerCallback):
+    def on_log(self, args, state: TrainerState, control: TrainerControl, logs=None, **kwargs):
+        """훈련 중 매 로그 이벤트 발생 시 호출됩니다."""
+        if logs is not None:
+            # train loss 기록
+            if "loss" in logs:
+                wandb.log({"train/loss": logs["loss"], "step": state.global_step})
 
-output_dir = "./llama"
+            # validation 평가 및 loss 기록 (평가 주기에 따라 실행됨)
+            if "eval_loss" in logs:
+                wandb.log({"eval/loss": logs["eval_loss"], "step": state.global_step})
 
-# 🔹 10. SFT Trainer 설정
+output_dir = "./instruction_facebook"
+
+# 🔹 9. SFT Trainer 설정
 trainer = SFTTrainer(
     model=model,
     train_dataset=dataset["train"],
     eval_dataset=dataset["validation"],
     args=SFTConfig(
         output_dir=output_dir,
-        per_device_train_batch_size=1,
-        per_device_eval_batch_size=1,
-        num_train_epochs=10000,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=20,
         logging_dir="./logs",
         logging_steps=100,
-        save_total_limit=0,
-        save_steps=0
+        eval_steps=100,
+        save_total_limit=1,
+        save_steps=500,
+        evaluation_strategy="steps",  # 평가 전략을 'steps'로 설정
+        load_best_model_at_end=True,  # 가장 낮은 평가 손실을 갖는 모델을 저장
+        metric_for_best_model="eval_loss",  # 가장 낮은 eval_loss를 기준으로 모델 선택
     ),
     formatting_func=formatting_prompts_func,
     data_collator=collator,
+    callbacks=[WandbLoggingCallback()]  # 콜백 추가
 )
 
 
-# 🔹 11. 학습 시작
+# 🔹 10. 학습 시작
 trainer.train()
 
 wandb.finish()
